4a416982312f2da2962a6059b4ea0c2eb8f1b6bd
Omkar-Ghodake <omkar.a.g.78@gmail.com>
2024-12-27 11:41:16 +0530

    completed landing page and created button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 const Button = ({ children, className }) => {
   return (
     <button
-      className={`bg-white text-black px-10 py-5 text-xl font-semibold tracking-wider`}
+      className={twMerge(
+        'bg-white text-black px-10 py-4 text-xl font-semibold tracking-wider flex items-center hover:scale-x-110 duration-150',
+        className
+      )}
     >
       {children}
     </button>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,33 +3,58 @@ import landingImage from '../assets/landing_detective.jpg'
 import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 import { GiPistolGun } from 'react-icons/gi'
+import { motion } from 'framer-motion'
+import { AiOutlineAim } from 'react-icons/ai'
 
 const Landing = () => {
   return (
     <div className='h-[90vh] w-full bg-transparent flex justify-between mt-[10vh] overflow-hidden'>
       <div className='w-fit p-20 pr-0 flex flex-col space-y-10'>
-        <div className='flex flex-col space-y-5'>
-          <h1 className='title text-9xl font-bold'>MPC</h1>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.1 }}
+          className='flex flex-col space-y-5'
+        >
+          <motion.h1 className='title text-9xl font-bold'>MPC</motion.h1>
 
-          <h2 className='text-6xl'>
+          <motion.h2 className='text-6xl'>
             Member of Parliament's <br /> Corner
-          </h2>
-        </div>
+          </motion.h2>
+        </motion.div>
 
-        <p className='text text-4xl text-red-400 font-bebas tracking-wider'>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, originX: 1 }}
+          transition={{ duration: 1.5 }}
+          className='text text-4xl text-red-400 font-bebas tracking-wider'
+        >
           Investigating the Seva trail: Every MP’s contribution revealed.
-        </p>
+        </motion.p>
 
-        <Link to={'/what-we-do'}>
-          <Button>
-            Uncover Our Mission <GiPistolGun />
-          </Button>
-        </Link>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <Link to={'/our-mission'}>
+            <Button>
+              Uncover Our Mission <AiOutlineAim className='ml-2 text-2xl' />
+            </Button>
+          </Link>
+        </motion.div>
       </div>
 
-      <div className='landing-image h-full'>
+      <motion.div
+        // initial={{ opacity: 0, x: '100%' }}
+        // animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 5 }}
+        className='landing-image h-full'
+      >
         <img src={landingImage} alt='' className='h-full' />
-      </div>
+      </motion.div>
     </div>
   )
 }
