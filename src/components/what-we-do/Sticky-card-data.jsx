6d4649689e47383f0230766b7bd06f505a21eef8
rishabhk5236 <rishabhk5236@gmail.com>
2025-01-03 17:53:56 +0530

    Added Card Modal

--- src/components/what-we-do/Sticky-card-data.jsx
+++ src/components/what-we-do/Sticky-card-data.jsx
@@ -6,12 +6,15 @@ import modiJiImage from "../../assets/WhatWeDo-Assets/modiJiImage.png";
 import pinLogo from "../../assets/WhatWeDo-Assets/pinLogo.png";
 import "../../styles/WhatWeDo/StickyNotes.css";
 
-
-
 export default function StickyCardData() {
+  const [index, setIndex] = useState();
 
-  const [index,setIndex]=useState();
+  const [modalOpen, setModalOpen] = useState(false);
 
+  const assignIndex = (i) => {
+    setIndex(i);
+    setModalOpen(true);
+  };
 
   const setDegree = () => {
     const degreeArray = [];
@@ -29,85 +32,95 @@ export default function StickyCardData() {
       title: "Jio TV",
       description:
         "We completed a round of testing for JioTV, which involved verifying app functionality, checking channel streaming quality, testing user interface responsiveness, ensuring device compatibility, and identifying bugs for a seamless user experience.",
+      totalScore: 8,
+      issueCount: 4,
       image: jioTvLogo,
     },
     {
       title: "Jio Cloud",
       description:
         "We completed a round of testing for JioTV, which involved verifying app functionality, checking channel streaming quality, testing user interface responsiveness, ensuring device compatibility, and identifying bugs for a seamless user experience.",
+      totalScore: "37",
+      issueCount: "11",
       image: jioCloudLogo,
     },
     {
       title: "Alexa MKB",
       description:
-        "We completed a round of testing for JioTV, which involved verifying app functionality, checking channel streaming quality, testing user interface responsiveness, ensuring device compatibility, and identifying bugs for a seamless user experience.",
+        "We reviewed the Alexa MKB user story, performed sanity testing, completed checklist tasks, and raised issues based on insights gained during the checklist evaluation.",
+      totalScore: "77",
+      issueCount: "12",
       image: alexaLogo,
     },
     {
       title: "MPC",
       description:
-        "We completed a round of testing for JioTV, which involved verifying app functionality, checking channel streaming quality, testing user interface responsiveness, ensuring device compatibility, and identifying bugs for a seamless user experience.",
+        "In MPC, we completed the citizen view verification task, Twitter stats of MP task, regional language checklist, and sanity testing, thoroughly reviewing and raising any issues encountered.",
+      totalScore: 2,
+      issueCount: 15,
       image: modiJiImage,
     },
   ];
 
   return (
-    <div className="container stickyCardData">
-    
-      {temp.map((element, index) => {
-        const degree = setDegree();
+    <>
+      <div className="container stickyCardData">
+        {temp.map((element, index) => {
+          const degree = setDegree();
 
-        return (
-          <>
-            <div
-              class="card "
-              data-bs-toggle="modal" data-bs-target="#stickyCardModal"
-              style={{
-                rotate: degree[index] + "deg",
-              }}
-              onClick={()=>setIndex(index)}
-            >
-              <span className="card-img-top">
-                <img src={element.image} class="" alt="..." />
-              </span>
-              <div class="card-body">
-                <p className="card-title">{element.title}</p>
-
-                {/* badge for pinning the photos */}
-                <span class="position-absolute top-0 start-50 p-3 translate-middle">
-                  <img src={pinLogo}/>
+          return (
+            <>
+              <div
+                class="card "
+                data-bs-toggle="modal"
+                data-bs-target="#stickyCardModal"
+                style={{
+                  rotate: degree[index] + "deg",
+                }}
+                onClick={() => assignIndex(index)}
+              >
+                <span className="card-img-top">
+                  <img src={element.image} class="" alt="..." />
                 </span>
+                <div class="card-body">
+                  <p className="card-title">{element.title}</p>
+
+                  {/* badge for pinning the photos */}
+                  <span class="position-absolute top-0 start-50 p-3 translate-middle">
+                    <img src={pinLogo} />
+                  </span>
+                </div>
               </div>
+
+              {/* this modal is for the data visualtization of small cards */}
+            </>
+          );
+        })}
+      </div>
+
+      {modalOpen && (
+        <div className="cardModal" onClick={() => setModalOpen(false)}>
+          <div className="dialoge bg-black/70 backdrop-blur-3xl">
+            <div className="image">
+              <img src={temp[index].image} alt=".." />
+              <h1>{temp[index].title}</h1>
+              <p className="text-center">{temp[index].description}</p>
+              <hr />
             </div>
 
-
-
-
-
-          </>
-        );
-      })}
-
-      {/* this modal is for the data visualtization of small cards */}
-
-{<div class="modal fade" id="stickyCardModal" tabindex="-1" aria-labelledby="stickyCardModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="stickyCardModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>}
-
-    </div>
+            <div className="score">
+              <span>
+                <h1>Total Score</h1>
+                <h1>{temp[index].totalScore}</h1>
+              </span>
+              <span>
+                <h1>Issues Found</h1>
+                <h1>{temp[index].issueCount}</h1>
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
