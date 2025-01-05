0458f5e011d4aea0fb8dc8c52ee13c76e0fb6da9
rishabhk5236 <rishabhk5236@gmail.com>
2025-01-05 19:51:00 +0530

    Added Images to Missions Page

--- src/components/what-we-do/NewsPaper.jsx
+++ src/components/what-we-do/NewsPaper.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
-import temp from "../../assets/WhatWeDo-Assets/temporary.png";
+import image1 from "../../assets/WhatWeDo-Assets/image1.png";
+import image2 from "../../assets/WhatWeDo-Assets/image2.png";
+import image3 from "../../assets/WhatWeDo-Assets/image3.png";
+import image4 from "../../assets/WhatWeDo-Assets/image4.png";
+
 import pinLogo from "../../assets/WhatWeDo-Assets/pinLogo.png";
 import "../../styles/WhatWeDo/NewsPaper.css";
 
@@ -16,7 +20,7 @@ export default function NewsPaper() {
           expand == "image1" ? setExpand(null) : setExpand("image1");
         }}
       >
-        <img src={temp} alt="" />
+        <img src={image1} alt="" />
         {/* badge for{expand==  pinning ? 'expand':'image1'}the photos */}
         <span class="pin position-absolute top-2 start-50 p-3 translate-middle">
           {!expand && <img src={pinLogo} />}
@@ -30,7 +34,7 @@ export default function NewsPaper() {
           expand == "image2" ? setExpand(null) : setExpand("image2");
         }}
       >
-        <img src={temp} alt="" />
+        <img src={image2} alt="" />
         {/* badge for pinning the photos */}
         <span class="pin position-absolute top-2 start-50 p-3 translate-middle">
           {!expand && <img src={pinLogo} />}
@@ -44,7 +48,7 @@ export default function NewsPaper() {
           expand == "image3" ? setExpand(null) : setExpand("image3");
         }}
       >
-        <img src={temp} alt="" />
+        <img src={image3} alt="" />
         {/* badge for pinning the photos */}
         <span class="pin position-absolute top-2 start-50 p-3 translate-middle">
           {!expand && <img src={pinLogo} />}
@@ -58,7 +62,7 @@ export default function NewsPaper() {
           expand == "image4" ? setExpand(null) : setExpand("image4");
         }}
       >
-        <img src={temp} alt="" />
+        <img src={image4} alt="" />
         {/* badge for pinning the photos */}
         <span class="pin position-absolute top-2 start-50 p-3 translate-middle">
           {!expand && <img src={pinLogo} />}
